6984706c77de28c46753cea28cdd72d6bd3d4351
hritik <86347286+hritikd3@users.noreply.github.com>
2022-02-16 11:59:32 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./App.css";
+import sketch from "./sketch";
+import p5 from "p5";
+
+const TEXT = "Happy Birthday Sahib";
+
+const App = () => {
+  const canvasRef = React.useRef(null);
+
+  React.useEffect(() => {
+    const canvasDivElement = canvasRef.current;
+
+    // NOTE: If you change TEXT value to something else, you'll need to make changes in sketch.js as well to make it work
+
+    new p5(sketch(canvasDivElement, TEXT), canvasDivElement);
+  });
+  const alertHandler = () => {
+    alert(
+      "ʕ•́ᴥ•̀ʔ Hritik is currently Smashing his keyboard until something good happens... 😄"
+    );
+  };
+  return (
+    <>
+      <div className="main">
+        <div ref={canvasRef} />
+      </div>
+      <footer className="footer">
+        <h2>
+          Made with ❤️ by <span onClick={alertHandler}>Hritik</span>{" "}
+        </h2>
+      </footer>
+    </>
+  );
+};
+
+export default App;
