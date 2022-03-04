0e948f4841ec85581477189bc97a6eb41d0c9d89
hritik <86347286+hritikd3@users.noreply.github.com>
2022-03-04 21:36:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import sketch from "./sketch";
 import p5 from "p5";
 
-const TEXT = "Happy Birthday Abhay";
+const TEXT = "Happy Birthday Bittoo";
 
 const App = () => {
   const canvasRef = React.useRef(null);
